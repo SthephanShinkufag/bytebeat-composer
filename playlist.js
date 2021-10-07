af69814d680e95586fa6e018477dd58ac28ffa6d
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-10-07 18:56:18 -0400

    made playlist parser somewhat more readable

--- playlist.js
+++ playlist.js
@@ -7,13 +7,13 @@ function processCodeHTML(code, sRate) {
 	if(typeof code !== 'string') {
 		code = code.join('@!@LINE_BREAK@!@');
 	}
-	return '<code' + (sRate ? ' samplerate="' + sRate + '"' : '') + '>' +
+	return `<code${ sRate ? ` samplerate="${ sRate }"` : '' }>${
 		code.replace(/&/g, '&amp;')
 			.replace(/>/g, '&gt;')
 			.replace(/</g, '&lt;')
 			.replace(/ /g, '&#xA0;')
-			.replace(/@!@LINE_BREAK@!@/g, '<br>\r\n') +
-		'</code>';
+			.replace(/@!@LINE_BREAK@!@/g, '<br>\r\n')
+	}</code>`;
 }
 
 function getEntryHTML(entry, isChildren) {
@@ -30,7 +30,7 @@ function getEntryHTML(entry, isChildren) {
 			}
 			authorHTML = authorsArr.join(', ');
 		}
-		authorHTML = descr ? ` (by ${ authorHTML })` : 'by ' + authorHTML;
+		authorHTML = descr ? ` (by ${ authorHTML })` : `by ${ authorHTML }`;
 	}
 	let descrHTML = '';
 	if(descr) {
@@ -43,14 +43,14 @@ function getEntryHTML(entry, isChildren) {
 	case 1: starHTML += ' <span class="star-white"></span>'; break;
 	case 2: starHTML += ' <span class="star-yellow"></span>';
 	}
-	const descr2HTML = entry.description2 ? ' - ' + entry.description2 : '';
-	const codeLen = !code ? 0 : '<span class="codelength">' +
-		(code instanceof Array ? code.join('').length : code.length) + 'B</span>';
-	const codeHTML = !code && !lCode ? '' :
-		(lCode ? `<a class="code-load" loadcode="${ lCode }"${
+	const descr2HTML = entry.description2 ? ` - ${ entry.description2 }` : '';
+	const codeLen = !code ? 0 : `<span class="codelength">${
+		code instanceof Array ? code.join('').length : code.length }B</span>`;
+	const codeHTML = !code && !lCode ? '' : (
+		lCode ? `<a class="code-load" loadcode="${ lCode }"${
 			sRate ? ` samplerate="${ sRate }"` : '' }>&#9658; Click to load  pretty code</a>${
-			code ? '</br>' : '' }` : '') +
-		(code ? processCodeHTML(code, sRate) + ' ' + codeLen : '');
+			code ? '</br>' : '' }` : ''
+	) + (code ? processCodeHTML(code, sRate) + ' ' + codeLen : '');
 	return isChildren ?
 		codeHTML + (descrHTML ? starHTML + ' ' + descrHTML : starHTML) + descr2HTML :
 		descrHTML + (code || lCode ? starHTML + '<br>\r\n' + codeHTML + descr2HTML : '');
@@ -70,7 +70,7 @@ function addPlaylist(obj, id) {
 		}
 		html.push(entryHTML.join('<br>\r\n'));
 	}
-	$id('library-' + id).innerHTML = `<ul><li>${ html.join('</li>\r\n<li>') }</li></ul>`;
+	$id(`library-${ id }`).innerHTML = `<ul><li>${ html.join('</li>\r\n<li>') }</li></ul>`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
