bf522cc3d37b1fe4f3a24cf3cd2c41de430ec9a6
SthephanShi <sthephan.shi@gmail.com>
2021-10-10 23:04:31 +0300

    Commit renamed library files

---
+++ library/RealZynx92 - cool loop i guess.js
@@ -0,0 +1,46 @@
+t2 = t / 44100,
+p = 1.05946309,
+
+// Notes
+a = 14080,
+as = a * p,
+b = as * p,
+c = b * p,
+cs = c * p,
+d = cs * p,
+ds = d * p,
+e = ds * p,
+f = e * p,
+fs = f * p,
+g = fs * p,
+gs = g * p,
+
+// Functions
+saw = function(n) {
+  return (t2 * n) & 63;
+},
+sqr = function(n) {
+  return (t2 * n) & 64;
+},
+sine = function(n) {
+  return 32 * sin((t2 * n) / 10.25) + 32;
+},
+
+// Percussion
+kick = 25 * (3E3 / (t & 16383) & 3),
+snare = 10 * (3E4 / ((t + 16383) & 32767) & 4),
+hihat = ((-t >> 8) % 16 + 16) % t * random(),
+
+// Arp chords
+arpch = [
+  c, e, g,
+  a, f, g,
+  a, c, e,
+  g / 2, a, c
+],
+
+// Bass
+bs = [g / 2, a / 2, c / 2, e / 2],
+
+// Output
+sine(bs[(t >> 16) % 4]) + sqr(arpch[((t >> 12) % 3) + ((t >> 16) % 4) * 3]) + kick + snare + hihat
