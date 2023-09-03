4dda5da00a46b17864a392126dec65c9b966c59b
SthephanShi <sthephan.shi@gmail.com>
2023-09-04 02:51:05 +0300

    Add Dusk theme

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -579,7 +579,7 @@ globalThis.bytebeat = new class {
 		this.audioCtx = new AudioContext({ latencyHint: 'balanced', sampleRate: 48000 });
 		this.audioGain = new GainNode(this.audioCtx);
 		this.audioGain.connect(this.audioCtx.destination);
-		await this.audioCtx.audioWorklet.addModule('./scripts/audioProcessor.mjs?version=2023080300');
+		await this.audioCtx.audioWorklet.addModule('./scripts/audioProcessor.mjs?version=2023090400');
 		this.audioWorkletNode = new AudioWorkletNode(this.audioCtx, 'audioProcessor',
 			{ outputChannelCount: [2] });
 		this.audioWorkletNode.port.addEventListener('message', e => this.receiveData(e.data));
